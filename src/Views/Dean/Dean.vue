de217f801f6182769ac03282f2d171f7e345e07d
Ken <20-08233@g.batstate-u.edu.ph>
2024-02-14 16:53:24 +0800

    Done All Tabs

---
+++ src/Views/Dean/Dean.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import Navigation from '../../components/Others/Navigation.vue';
+    import deanNav from '../../components/Others/deanNav.vue';
+</script>
+
+<template>
+    <main class="w-full flex flex-col items-center">
+
+        <Navigation/>
+        <deanNav/>
+
+
+    </main>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,9 @@ export default {
 
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
 }
 
